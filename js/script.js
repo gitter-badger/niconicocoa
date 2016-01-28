6ef96290de0b0b616237302840c549ff81bd40a3
nabettu <tatsuaki.w@gmail.com>
2016-01-28 16:59:47 +0900

    init

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var milkcocoa = new MilkCocoa('yieldijxl4pvn.mlkcca.com');
+var ds = milkcocoa.dataStore('messages');
+
+$("#js-submitBtn").on("click", function() {
+  var sendtext = $("#js-inputText").val();
+  console.log("send" + sendtext);
+
+  ds.push({
+    content: sendtext
+  });
+})
+
+ds.on('send', function(event) {
+  console.log(event.value.content);
+});
